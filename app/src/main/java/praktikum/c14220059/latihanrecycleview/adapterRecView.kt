6c8b380ddf20bdbaddff2875c2dd3b86cefe05dc
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2024-11-19 19:29:36 +0700

    commit 2

--- app/src/main/java/praktikum/c14220059/latihanrecycleview/adapterRecView.kt
+++ app/src/main/java/praktikum/c14220059/latihanrecycleview/adapterRecView.kt
@@ -4,19 +4,31 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
 class adapterRecView (private val listWayang: ArrayList<wayang>) : RecyclerView
     .Adapter<adapterRecView.ListViewHolder>() {
+
+        private lateinit var onItemClickCallback: OnItemClickCallback
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: wayang)
+
+        fun delData(pos: Int)
+    }
+
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var _namaWayang = itemView.findViewById<TextView>(R.id.namaWayang)
         var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayang)
         var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
         var _gambarWayang = itemView.findViewById<ImageView>(R.id.gambarWayang)
+        var _btnHapus = itemView.findViewById<Button>(R.id.btnHapus)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -24,6 +36,9 @@ class adapterRecView (private val listWayang: ArrayList<wayang>) : RecyclerView
         return ListViewHolder(view)
     }
 
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
 
     override fun getItemCount(): Int {
         return listWayang.size
@@ -37,5 +52,14 @@ class adapterRecView (private val listWayang: ArrayList<wayang>) : RecyclerView
         holder._deskripsiWayang.text = wayang.deskripsi
         Log.d("TEST", wayang.foto)
         Picasso.get().load(wayang.foto).into(holder._gambarWayang)
+
+        holder._gambarWayang.setOnClickListener {
+//            Toast.makeText(holder.itemView.context, wayang.nama, Toast.LENGTH_LONG).show()
+            onItemClickCallback.onItemClicked(listWayang[position])
+        }
+
+        holder._btnHapus.setOnClickListener {
+            onItemClickCallback.delData(position)
+        }
     }
 }
--- app/src/main/java/praktikum/c14220059/latihanrecycleview/wayang.kt
+++ app/src/main/java/praktikum/c14220059/latihanrecycleview/wayang.kt
@@ -1,8 +1,12 @@
 package praktikum.c14220059.latihanrecycleview
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class wayang(
     var foto: String,
     var nama: String,
     var karakter: String,
     var deskripsi: String
-)
+) : Parcelable
